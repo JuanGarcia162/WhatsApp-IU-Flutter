361f5f4afe14f2138d443e0684dc91bd60741f51
JuanGarcia162 <juadgarca@misena.edu.co>
2022-08-15 21:31:06 -0500

    first commit

---
+++ lib/info.dart
@@ -0,0 +1,95 @@
+const info = [
+  {
+    'name': 'Elon Musk',
+    'message': 'Mucho texto despues',
+    'time': '11:17 am',
+    'profilePic':
+        'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
+  },
+  {
+    'name': 'Contacto 2',
+    'message': 'Hello, whats up',
+    'time': '2:25 pm',
+    'profilePic':
+        'https://www.socialketchup.in/wp-content/uploads/2020/05/fi-vill-JOHN-DOE.jpg',
+  },
+  {
+    'name': 'Contacto 3',
+    'message': 'Hello, I want to sleep.',
+    'time': '1:03 pm',
+    'profilePic':
+        'https://media.cntraveler.com/photos/60596b398f4452dac88c59f8/16:9/w_3999,h_2249,c_limit/MtFuji-GettyImages-959111140.jpg',
+  },
+  {
+    'name': 'Contacto 4',
+    'message': 'Call me, have some work',
+    'time': '12:06 pm',
+    'profilePic':
+        'https://pbs.twimg.com/profile_images/1419974913260232732/Cy_CUavB.jpg',
+  },
+  {
+    'name': 'Contacto 5',
+    'message': 'You ate food?',
+    'time': '10:00 am',
+    'profilePic':
+        'https://uploads.dailydot.com/2018/10/olli-the-polite-cat.jpg?auto=compress%2Cformat&ixlib=php-3.3.0',
+  },
+  {
+    'name': 'Contacto 6',
+    'message': 'Yo!!!!! Long time, no see!?',
+    'time': '9:53 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Contacto 7',
+    'message': 'Am I fat?',
+    'time': '7:25 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Contacto 8',
+    'message': 'I am from International Olym...',
+    'time': '6:02 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Contacto 9',
+    'message': 'Lets Code!',
+    'time': '4:56 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1619194617062-5a61b9c6a049?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHJhbmRvbSUyMHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    'name': 'Contacto 10',
+    'message': 'Images by Unsplash',
+    'time': '1:00 am',
+    'profilePic':
+        'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+];
+
+const messages = [
+  {"isMe": false, "text": "Mucho Texto", "time": "10:00 am"},
+  {"isMe": true, "text": "Mucho Texto", "time": "11:00 am"},
+  {"isMe": false, "text": "Mucho Texto", "time": "11:01 am"},
+  {"isMe": true, "text": "Mucho Texto", "time": "11:03 am"},
+  {"isMe": false, "text": "Mucho Texto", "time": "11:04 am"},
+  {"isMe": true, "text": "Mucho Texto", "time": "11:05 am"},
+  {"isMe": false, "text": "Mucho Texto", "time": "11:05 am"},
+  {"isMe": true, "text": "Mucho Texto", "time": "11:06 am"},
+  {
+    "isMe": false,
+    "text": "Mucho Texto",
+    "time": "11:06 am",
+  },
+  {
+    "isMe": true,
+    "text": "Mucho Texto",
+    "time": "11:15 am",
+  },
+  {"isMe": false, "text": "Mucho Texto despues", "time": "11:17 am"},
+  {"isMe": true, "text": "Mucho mucho Texto despues", "time": "11:17 am"},
+];
